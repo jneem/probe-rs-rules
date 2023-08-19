3883348c08eb375f9f658b97e8c3d293c7bc1691
Joe Neeman <joeneeman@gmail.com>
2023-08-19 14:39:12 -0500

    Clean up a little

--- flake.nix
+++ flake.nix
@@ -1,38 +1,41 @@
 {
   description = "Udev rules for JTAG and similar debug probes";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: 
-  let 
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-  in
-  rec {
-    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-      name = "probe-rs-rules";
-      src = self;
-      installPhase = ''
-        mkdir -p $out/lib/udev/rules.d
-        cp 69-probe-rs.rules $out/lib/udev/rules.d/
-      '';
-    };
-
-    nixosModules.default = { config, pkgs, lib, ... }:
-    let
-      inherit (lib) mkOption mkIf types;
-      cfg = config.hardware.probe-rs;
-    in
-    {
-      options.hardware.probe-rs = {
-        enable = mkOption {
-          type = types.bool;
-          default = false;
-          description = lib.mkDoc "TODO";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "probe-rs-rules";
+          src = self;
+          installPhase = ''
+            mkdir -p $out/lib/udev/rules.d
+            cp 69-probe-rs.rules $out/lib/udev/rules.d/
+          '';
         };
-      };
-      config = mkIf cfg.enable {
-        services.udev.packages = [ packages.x86_64-linux.default ];
-      };
-    };
-  };
+
+        nixosModules.default = { config, pkgs, lib, ... }:
+        let
+          inherit (lib) mkOption mkIf types;
+          cfg = config.hardware.probe-rs;
+        in
+        {
+          options.hardware.probe-rs = {
+            enable = mkOption {
+              type = types.bool;
+              default = false;
+              description = lib.mkDoc "Add udev permissions for various probes.";
+            };
+          };
+          config = mkIf cfg.enable {
+            services.udev.packages = [ packages.default ];
+          };
+        };
+      }
+  );
 }
